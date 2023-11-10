73d5b1d5fb2d9b07ef1385ce4d4f25a3bc32b6dd
Victor Oscuro <84464026+victoroscuro@users.noreply.github.com>
2023-11-10 17:03:49 +0100

    Update sitesPage.js

--- pages/sitesPage.js
+++ pages/sitesPage.js
@@ -13,8 +13,8 @@ class SitesPage {
         await this.page.frameLocator('iframe[title="Add Site"]').getByLabel('Name').fill(miniumName);
         await this.page.frameLocator('iframe[title="Add Site"]').getByLabel('Name').click();
         await this.page.frameLocator('iframe[title="Add Site"]').getByRole('button', { name: 'Add' }).click();
-        await expect(this.page.frameLocator('iframe[title="Add Site"]').getByText('The creation of the site may take some time. Closing the window will not cancel')).toBeHidden({ timeout: 20000 });
-        await expect(this.page.getByRole('heading', { name: 'Site Settings' })).toBeVisible({ timeout: 10000 });
+        await expect(this.page.frameLocator('iframe[title="Add Site"]').getByText('The creation of the site may take some time. Closing the window will not cancel')).toBeHidden({ timeout: 40000 });
+        await expect(this.page.getByRole('heading', { name: 'Site Settings' })).toBeVisible({ timeout: 15000 });
         await expect(this.page.getByRole('link', { name: 'Catalog' })).toBeVisible();
     }
   }
