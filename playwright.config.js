f77bad74f95b7d552ec28b25a474922e128938ff
Victor Oscuro <84464026+victoroscuro@users.noreply.github.com>
2023-11-10 17:03:10 +0100

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -18,6 +18,7 @@ module.exports = defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  timeout: 60000,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
