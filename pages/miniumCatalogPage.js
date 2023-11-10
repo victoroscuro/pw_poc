caf7fc929f3d75e14fbe544b08127dd76dc5f496
victoroscuro <victor.oscuro@liferay.com>
2023-11-10 17:40:14 +0100

    add steps to test

--- pages/miniumCatalogPage.js
+++ pages/miniumCatalogPage.js
@@ -12,5 +12,13 @@ class MiniumCatalogPage {
     await this.page.locator(locator).click();
   }
 
+  async checkAccountOnAccountSelector(accountName) {
+    await expect(this.page.getByRole('button', { name: 'B2BAccount1' })).toContainText(accountName);
+  }
+
+  async checkProductInMiniCart(product) {
+    await this.page.getByRole('button', { name: '1', exact: true }).click();
+    await expect(this.page.getByRole('link', { name: 'thumbnail U-Joint MIN55861' })).toContainText(product);
+  }
 };
 export default MiniumCatalogPage;
