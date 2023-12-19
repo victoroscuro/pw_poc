8a1c7085f49ebb3a14f9719e4888883c54c8ee1b
victoroscuro <victor.oscuro@liferay.com>
2023-12-19 11:03:05 +0100

    move assertions to test case

--- pages/miniumCatalogPage.js
+++ pages/miniumCatalogPage.js
@@ -11,14 +11,5 @@ class MiniumCatalogPage {
     const locator = await utils.resolvePath(miniumCatalogPage.anyProductAddToCartButton, productToBuy);
     await this.page.locator(locator).click();
   }
-
-  async checkAccountOnAccountSelector(accountName) {
-    await expect(this.page.getByRole('button', { name: 'B2BAccount1' })).toContainText(accountName);
-  }
-
-  async checkProductInMiniCart(product) {
-    await this.page.getByRole('button', { name: '1', exact: true }).click();
-    await expect(this.page.getByRole('link', { name: 'thumbnail U-Joint MIN55861' })).toContainText(product);
-  }
 };
 export default MiniumCatalogPage;
